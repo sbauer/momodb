a358cf5872e507c40a5c038979f11e10cd7f249d
Shane Bauer <shane@shanebauer.com>
2012-06-01 16:29:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Momodb::Application.config.secret_token = '4cc4147cd8ebfb4bd8ee75a316e31fceb4c56fa65d9f8f259ddad076594d14e40319129608028e2fbaaa9315033487f14557ae1ee3222f356ffd1f9ab96440df'

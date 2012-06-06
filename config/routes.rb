b1b3f2ee2bb0ed0c2e1a8d1eb2780b03f0f96707
Shane Bauer <shane@shanebauer.com>
2012-06-06 13:36:21 -0400

    Added custom devise routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Momodb::Application.routes.draw do
   
-  devise_for :users
+  devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
   root :to => 'home#index'
 

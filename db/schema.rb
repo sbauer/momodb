be973e48708eada6725d4397f60a2bbe0750cc01
Shane Bauer <shane@shanebauer.com>
2012-06-15 16:20:30 -0400

    Added user profile with basic fields.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120606185013) do
+ActiveRecord::Schema.define(:version => 20120615201315) do
+
+  create_table "user_profiles", :force => true do |t|
+    t.string   "name"
+    t.date     "birth_date"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
